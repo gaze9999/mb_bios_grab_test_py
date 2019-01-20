026e3c02cb599cce9cb9b8aa2535515f748829b6
gaze9999 <gaze9999@livemail.tw>
2019-01-21 05:02:32 +0800

    adding_notes

--- grab_mb.py
+++ grab_mb.py
@@ -2,12 +2,10 @@ import os
 import requests
 from bs4  import BeautifulSoup
 
-
-
 #absolute dir the script is in
 script_dir = os.path.dirname(__file__)
-opt_path = "./"
-file_path = os.path.join(script_dir, opt_path)
+opt_path   = "./"
+file_path  = os.path.join(script_dir, opt_path)
 
 #create folders
 if not os.path.exists(file_path + "opt"):
@@ -21,27 +19,29 @@ Msi_B360 = file=open(file_path + "./msi/B360.html", "r", encoding = "utf8")
 Msi_Z370 = file=open(file_path + "./msi/Z370.html", "r", encoding = "utf8")
 Msi_Z390 = file=open(file_path + "./msi/Z390.html", "r", encoding = "utf8")
 cname    = "productcard-link" #class_name
-mbl1     = []
+MBList1  = []
+
+#for print all
 Msi_List = [Msi_H310, Msi_H370, Msi_B360, Msi_Z370, Msi_Z390]
 Msi_len  = len(Msi_List)
 
-for br in Msi_List:
-    ord = 0
-    if ord != len(Msi_List):
-        Msi_List.pop(ord)
-        soup = BeautifulSoup(br, "html.parser")
-        ord += 1
-        for link in soup.find_all("a", href=True):
+for board in Msi_List:
+    order = 0 #set list
+    if order != len(Msi_List):
+        Msi_List.pop(order) #picking
+        soup = BeautifulSoup(board, "html.parser")
+        order += 1
+        for link in soup.find_all("a", href=True): #getting links
             link = link.get('href')
-            mbl1.append(link)
+            MBList1.append(link)
 
 #sorting and remove duplicated
-mbl2     = sorted(set(mbl1),key=mbl1.index)
+MBList2  = sorted(set(MBList1),key=MBList1.index)
 
 #print out resaults
 def output():
     with open(file_path + "./opt/output.json", "w") as f:
-        for item in mbl2:
+        for item in MBList2:
             f.write("%s\n" % item)
 
 #okay.
