674777931914a01d11000ad5dcb4b9cc5bd142e0
User <gaze9999@gmail.com>
2019-01-20 20:28:28 +0800

    relocate_dir

--- grab_mb.py
+++ grab_mb.py
@@ -2,18 +2,24 @@ import os
 import requests
 from bs4  import BeautifulSoup
 
+
+
 #absolute dir the script is in
 script_dir = os.path.dirname(__file__)
-msi_path = "msi/"
-opt_path = "opt/"
-msi_file_path = os.path.join(script_dir, msi_path)
-opt_file_path = os.path.join(script_dir, opt_path)
+opt_path = "./"
+file_path = os.path.join(script_dir, opt_path)
 
-Msi_H310 = file=open(msi_file_path + "H310.html", "r", encoding = "utf8")
-Msi_H370 = file=open(msi_file_path + "H370.html", "r", encoding = "utf8")
-Msi_B360 = file=open(msi_file_path + "B360.html", "r", encoding = "utf8")
-Msi_Z370 = file=open(msi_file_path + "Z370.html", "r", encoding = "utf8")
-Msi_Z390 = file=open(msi_file_path + "Z390.html", "r", encoding = "utf8")
+#create folders
+if not os.path.exists(file_path + "opt"):
+    os.makedirs(file_path       + "opt")
+if not os.path.exists(file_path + "msi"):
+    os.makedirs(file_path       + "msi")
+
+Msi_H310 = file=open(file_path + "./msi/H310.html", "r", encoding = "utf8")
+Msi_H370 = file=open(file_path + "./msi/H370.html", "r", encoding = "utf8")
+Msi_B360 = file=open(file_path + "./msi/B360.html", "r", encoding = "utf8")
+Msi_Z370 = file=open(file_path + "./msi/Z370.html", "r", encoding = "utf8")
+Msi_Z390 = file=open(file_path + "./msi/Z390.html", "r", encoding = "utf8")
 cname    = "productcard-link" #class_name
 mbl1     = []
 Msi_List = [Msi_H310, Msi_H370, Msi_B360, Msi_Z370, Msi_Z390]
@@ -34,7 +40,7 @@ mbl2     = sorted(set(mbl1),key=mbl1.index)
 
 #print out resaults
 def output():
-    with open(opt_file_path + "output.json", "w") as f:
+    with open(file_path + "./opt/output.json", "w") as f:
         for item in mbl2:
             f.write("%s\n" % item)
 
